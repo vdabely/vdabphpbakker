6edcdb1f8391b556f9e507e6408fdcc70622b09e
vdabely <scoofy@gmail.com>
2015-10-28 15:35:26 +0100

    28/10

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ setlocale(LC_TIME, 'NL_nl');
 require_once("business/productservice.php");
 require_once("business/klantenservice.php");
 require_once("business/bestellingservice.php");
-require_once("business/bestelservice.php");
+require_once("business/bestelregelservice.php");
 require_once("business/loginservice.php");
 
 session_start();
@@ -19,5 +19,12 @@ $arrProducten = productService::getAllProducts();
 // MAAK WINKELKAR AAN
 
 include 'presentation/homepage.php';
+/*
+Uw Login gegevens.
+Login : scoofy@gmail.com
+Paswoord : aXBvnR 
 
+Login : ely@telenet.be
+Paswoord : SqXrkn
+ */
 ?>
